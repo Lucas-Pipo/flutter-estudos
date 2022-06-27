322ebbfa2d63a986887bfc09216d1141f6975da2
Lucas-Pipo <83315224+Lucas-Pipo@users.noreply.github.com>
2022-06-27 19:27:14 -0300

    Primeiros códigos de projetos teste
    UHUL

--- meus_testes/lib/main.dart
+++ meus_testes/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aplicativo de Teste',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
