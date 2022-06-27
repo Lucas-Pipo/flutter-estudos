656d884f4211a9811da09bc7451b0280e63cf849
Lucas-Pipo <83315224+Lucas-Pipo@users.noreply.github.com>
2022-06-27 18:37:42 -0300

    Novos testes e horizontes

---
+++ meus_testes/android/app/src/main/kotlin/com/example/meus_testes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meus_testes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ meus_testes/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplicativo de Teste',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: const PrimeiraTela(),
+    );
+  }
+}
+
+class PrimeiraTela extends StatefulWidget {
+  const PrimeiraTela({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<PrimeiraTela> createState() => MinhaTelaInicial();
+}
+
+class MinhaTelaInicial extends State<PrimeiraTela> {
+  int contador = 0;
+
+  void _incrementarContador() {
+    setState(() {
+      contador--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Funciona?'),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Você apertou o botão $contador vezes!'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementarContador,
+        tooltip: 'Subtrair',
+        child: const Icon(Icons.remove),
+      ),
+    );
+  }
+}
